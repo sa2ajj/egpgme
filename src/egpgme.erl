ae962222b38243244ae6dd121fcc7df185331e24
Mikhail Sobolev <mss@mawhrin.net>
2011-11-10 11:05:44 +0200

    slightly changed the way so name is determined

--- src/egpgme.erl
+++ src/egpgme.erl
@@ -9,12 +9,12 @@
 -define(NOT_LOADED, error(nif_not_loaded)).
 
 init() ->
-    case code:priv_dir(egpgme) of
+    SoName = case code:priv_dir(egpgme) of
         {error, bad_name} ->
-            SoName = filename:join("../priv", ?NIF_NAME);
+            filename:join("../priv", ?NIF_NAME);
 
         Dir ->
-            SoName = filename:join(Dir, ?NIF_NAME)
+            filename:join(Dir, ?NIF_NAME)
     end,
     ok = erlang:load_nif(SoName, 0).
 
